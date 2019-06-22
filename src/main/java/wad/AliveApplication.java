78ee88378881d302fd0f975ca58b963b6f33dffa
Ville Ahonen <ville.ahonen@tuni.fi>
2019-06-22 20:22:42 +0300

    heroku address added

--- src/main/java/wad/AliveApplication.java
+++ src/main/java/wad/AliveApplication.java
@@ -19,6 +19,6 @@ public class AliveApplication {
     }
 
     public static String herokuUrl() {
-        return "";
+        return "https://dashboard.heroku.com/apps/salty-thicket-74659";
     }
 }
