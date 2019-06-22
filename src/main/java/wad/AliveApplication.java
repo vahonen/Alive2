17b3313400fed2f12850d38a3f102296e7780192
Ville Ahonen <ville.ahonen@tuni.fi>
2019-06-22 19:13:02 +0300

    git url added

--- src/main/java/wad/AliveApplication.java
+++ src/main/java/wad/AliveApplication.java
@@ -11,7 +11,7 @@ public class AliveApplication {
     }
 
     public static String githubUrl() {
-        return "";
+        return "https://github.com/vahonen/Alive.git";
     }
 
     public static String travisUrl() {
